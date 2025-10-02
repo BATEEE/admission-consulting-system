8c789f5c96c4c63fb34b878bbb07675ed7e7e378
Thang <thangtedy@gmail.com>
2025-10-02 15:31:54 +0700

    update cors

--- career-orientation/career-service/src/main/java/com/ttt/careerservice/config/WebConfig.java
+++ career-orientation/career-service/src/main/java/com/ttt/careerservice/config/WebConfig.java
@@ -12,11 +12,13 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") //
-                        .allowedOrigins("http://localhost:5173") //
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://admission-consulting-system-client.onrender.com"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*").allowCredentials(true);
             }
         };
     }
